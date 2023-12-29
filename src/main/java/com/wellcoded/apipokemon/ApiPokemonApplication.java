6ae27caa801f3f32f59c8b5b5132227259d26160
Welton Carvalho <welton.carvalho.dev@gmail.com>
2023-12-28 22:32:25 -0300

    initial commit

---
+++ src/main/java/com/wellcoded/apipokemon/ApiPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.wellcoded.apipokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPokemonApplication.class, args);
+	}
+
+}
